9073bbba6138dbc27dd8d97f184ab06f97ca1504
Rajat Sahani <rajatsahani272@gmail.com>
2026-04-08 07:32:26 +0530

    final structure fix

--- Frontend/js/main.js
+++ Frontend/js/main.js
@@ -3,14 +3,14 @@ document.addEventListener("click", (e) => {
 
   //  Login button
   if (e.target.classList.contains("login-button")) {
-    window.location.href = "/pages/login.html";
+    window.location.href = "./pages/login.html";
   }
   
   // card-img
   if (e.target.classList.contains("card-1")){
-    window.location.href="/pages/ask-doubt.html";
+    window.location.href="./pages/ask-doubt.html";
   }
    if (e.target.classList.contains("card-3")){
-    window.location.href="/pages/resources.html";
+    window.location.href="./pages/resources.html";
   }
 });
