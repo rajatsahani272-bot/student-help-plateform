80b310e9f640e01b0998396337c379e9fbf4e43c
Rajat Sahani <rajatsahani272@gmail.com>
2026-04-08 11:12:31 +0530

    fix api usage

--- Frontend/js/auth.js
+++ Frontend/js/auth.js
@@ -1,9 +1,10 @@
 // js/auth.js
 import BASE_URL from "./api.js";
 
+
 // REGISTER
 export async function registerUser(name, email, password) {
-  const res = await fetch("https://student-help-plateform.onrender.com/auth/register", {
+  const res = await fetch(`${BASE_URL}/auth/register`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
@@ -16,17 +17,13 @@ export async function registerUser(name, email, password) {
 
 // LOGIN
 export async function loginUser(email, password) {
-  const res = await fetch("https://student-help-plateform.onrender.com/auth/login", {
+  const res = await fetch(`${BASE_URL}/auth/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({
-      email,
-      password
-    })
+    body: JSON.stringify({ email, password })
   });
 
-  const data = await res.json(); 
-  return data; 
+  return res.json();
 }
